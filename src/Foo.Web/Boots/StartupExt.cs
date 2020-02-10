a07be9e6dfc2654ab4904e20be6092842a443149
congzw <2981810300@qq.com>
2020-02-10 10:10:29 +0800

    refactor startup

---
+++ src/Foo.Web/Boots/StartupExt.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Foo.Web.Boots
+{
+    public static partial class StartupExt
+    {
+    }
+}
--- src/Foo.Web/Startup.cs
+++ src/Foo.Web/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Foo.Web.Boots;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Foo.Web
@@ -9,12 +11,33 @@ namespace Foo.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddBasic();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            var mvcBuilder = services.AddMvc(opts =>
+            {
+                //Here it is being added globally. Could be used as attribute on selected controllers instead
+                //opts.Filters.Add(new MyJSONExceptionFilter());
+            });
+            mvcBuilder.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseBasic(env);
+            app.UseMyErrorHandling(env);
+            app.UseMyStaticFiles();
+
+            app.UseCookiePolicy();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
