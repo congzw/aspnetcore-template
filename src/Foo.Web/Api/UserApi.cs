4276a777a46e331328c7357c721a06bf0f4ba326
congzw <2981810300@qq.com>
2020-07-02 11:52:09 +0800

    add api demo for base/sub type convert

---
+++ src/Foo.Web/Api/UserApi.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foo.Web.Api
+{
+    [Route("Api/User")]
+    public class UserApi : BaseFooApi
+    {
+        [HttpGet("GetUsers")]
+        public IEnumerable<dynamic> GetUsers([FromQuery]GetUsersArgs args)
+        {
+            var users = new List<User>();
+            users.Add(new Teacher() { Username = "T1", Bar = "Bar"});
+            users.Add(new Student() { Username = "S1", Foo = "Foo"});
+
+            if (args == null || string.IsNullOrEmpty(args.UserType))
+            {
+                return users;
+            }
+
+            var query = users.AsEnumerable();
+            query = query.Where(x => args.UserType == x.UserType);
+            return query.ToList();
+        }
+    }
+
+    public class GetUsersArgs
+    {
+        public string UserType { get; set; }
+    }
+
+    public class User
+    {
+        public User()
+        {
+            UserType = this.GetType().Name;
+        }
+        public string Username { get; set; }
+        public string UserType { get; set; }
+    }
+
+    public class Student : User
+    {
+        public string Foo { get; set; }
+    }
+    public class Teacher : User
+    {
+        public string Bar { get; set; }
+    }
+}
